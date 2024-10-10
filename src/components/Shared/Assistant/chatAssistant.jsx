a83323e57888fab978476906963954d65d556ac8
Ashutosh thakur <ashutoshth456@gmail.com>
2024-10-11 00:54:19 +0530

    hard copy backend

--- src/components/Shared/Assistant/chatAssistant.jsx
+++ src/components/Shared/Assistant/chatAssistant.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 import styles from "./chatAssiatance.module.css";
-const SOCKET_SERVER_ENDPOINT = process.env.SOCKET_SERVER_ENDPOINT;
+const SOCKET_SERVER_ENDPOINT = "http://43.204.149.220:5050/";
 
 let socket;
 
